d868fd432228eb9b152b97614bf253c389582f2b
BenjaUrquia <urq23001@byui.edu>
2025-10-04 15:54:29 -0300

    W05: Chamber Discover Page
    - Images added
    - Images links added to the json
    - discover.html added
    - All links updated in the pages

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("data/places.json")
+        .then(response => response.json())
+        .then(data => displayPlaces(data))
+        .catch(error => console.error("Error loading JSON:", error));
+
+    function displayPlaces(places) {
+        const div = document.getElementById("discover-div");
+        const list = document.createElement("ul");
+
+        places.forEach(place => {
+            const li = document.createElement("li");
+            li.classList.add("place-card");
+
+            li.innerHTML = `
+                <h2>${place.title}</h2>
+                <figure>
+                    <img src="${place.image}" alt="${place.title}">
+                </figure>
+                <address>${place.address}</address>
+                <p>${place.description}</p>
+                <button class="learn-btn">Learn More</button>
+            `;
+
+            list.appendChild(li);
+        });
+
+        div.appendChild(list);
+    }
+
+
+    const messageDiv = document.getElementById("visit-message");
+    const lastVisit = localStorage.getItem("lastVisit");
+    const now = Date.now();
+
+    if (!lastVisit) {
+        messageDiv.textContent = "Welcome! Let us know if you have any questions.";
+    } else {
+        const msInDay = 1000 * 60 * 60 * 24;
+        const daysSince = Math.floor((now - parseInt(lastVisit)) / msInDay);
+
+        if (daysSince < 1) {
+            messageDiv.textContent = "Back so soon! Awesome!";
+        } else if (daysSince === 1) {
+            messageDiv.textContent = "You last visited 1 day ago.";
+        } else {
+            messageDiv.textContent = `You last visited ${daysSince} days ago.`;
+        }
+    }
+
+    // Store the current visit time
+    localStorage.setItem("lastVisit", now);
+});
